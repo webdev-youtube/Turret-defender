545f7aa5b5c91c5ee6f214442ddd3f03c0305041
webdev-youtube <144796747+webdev-youtube@users.noreply.github.com>
2026-01-16 13:38:15 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import { GameEngine } from './GameEngine.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('game-canvas');
-  const game = new GameEngine(canvas);
-});
